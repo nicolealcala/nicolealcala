bfdaa2f0dab1ae9bff07173c8bd854929d6463b0
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-01 17:23:30 +0800

    added shadow on back to top button

--- src/components/global/back-to-top.tsx
+++ src/components/global/back-to-top.tsx
@@ -1,15 +1,18 @@
 "use client";
 import React, { useContext } from "react";
 import { ArrowUp } from "lucide-react";
-import { ScrollContext } from "../context-providers/scroll-context";
+import { ScrollContext } from "../context-providers/scroll-context-provider";
+
 const BackToTop = () => {
   const isScrolled = useContext(ScrollContext);
+
   const handleBackToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
+
   return (
     <div
-      className={`fixed right-20 bottom-10 flex items-center justify-center rounded-full bg-yellow w-[50px] h-[50px] cursor-pointer transition-opacity duration-300 ${
+      className={`fixed right-20 bottom-10 flex items-center justify-center rounded-full bg-yellow w-[50px] h-[50px] cursor-pointer transition-opacity duration-300 shadow-xl z-50 ${
         isScrolled
           ? "opacity-100 pointer-events-auto"
           : "opacity-0 pointer-events-none"
