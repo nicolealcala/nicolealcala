99e09f6f01b5479b943262df610f14892da7d53b
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-01 17:30:14 +0800

    theme configuration

--- src/components/texts/details.tsx
+++ src/components/texts/details.tsx
@@ -1,14 +1,19 @@
-import React, { ReactNode } from "react";
+"use client";
+import React, { ReactNode, useContext } from "react";
+import { ThemeContext } from "../context-providers/theme-context-provider";
 
 const Details: React.FC<{
   subtitle: string;
   description: string;
   children?: ReactNode;
 }> = ({ subtitle, description, children }) => {
+  const theme = useContext(ThemeContext);
   return (
-    <div className="flex flex-col gap-y-4 max-w-[360px]">
-      <p className="font-semibold">{subtitle}</p>
-      <p>{description}</p>
+    <div className={`flex flex-col gap-y-4 max-w-[360px]`}>
+      <p className={`font-semibold ${theme === "light" && "text-black"}`}>
+        {subtitle}
+      </p>
+      <p className={theme === "light" ? "text-black" : ""}>{description}</p>
       {children}
     </div>
   );
