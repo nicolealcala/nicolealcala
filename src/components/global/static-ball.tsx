bbcc9039947a3a09dea07286fe29714713abf37c
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-01 17:23:45 +0800

    added blue and cyan static balls

--- src/components/global/static-ball.tsx
+++ src/components/global/static-ball.tsx
@@ -1,30 +1,51 @@
 import React from "react";
 
-const StaticBall = () => {
-  return (
-    <svg
-      width="600"
-      height="600"
-      viewBox="0 0 630 630"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="315" cy="315" r="315" fill="url(#paint0_linear_693_1135)" />
-      <defs>
-        <linearGradient
-          id="paint0_linear_693_1135"
-          x1="125.042"
-          y1="47.3564"
-          x2="753.446"
-          y2="537.948"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#08FBF5" />
-          <stop offset="0.691592" stopColor="#FF6392" />
-        </linearGradient>
-      </defs>
-    </svg>
-  );
-};
+export const CyanStaticBall = () => (
+  <svg
+    width="600"
+    height="600"
+    viewBox="0 0 630 630"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="315" cy="315" r="315" fill="url(#paint0_linear_693_1135)" />
+    <defs>
+      <linearGradient
+        id="paint0_linear_693_1135"
+        x1="125.042"
+        y1="47.3564"
+        x2="753.446"
+        y2="537.948"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#08FBF5" />
+        <stop offset="0.691592" stopColor="#FF6392" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
 
-export default StaticBall;
+export const BlueStaticBall = () => (
+  <svg
+    width="600"
+    height="600"
+    viewBox="0 0 630 630"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="315" cy="315" r="315" fill="url(#paint0_linear_847_9)" />
+    <defs>
+      <linearGradient
+        id="paint0_linear_847_9"
+        x1="125.042"
+        y1="47.3564"
+        x2="753.446"
+        y2="537.948"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#82C8F9" />
+        <stop offset="0.691592" stopColor="#FF6392" />
+      </linearGradient>
+    </defs>
+  </svg>
+);
