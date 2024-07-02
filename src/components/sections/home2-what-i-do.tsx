2f618c8eb0d053c92d9387f9d813991129e9a3c0
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-02 15:24:23 +0800

    configured themes switch

--- src/components/sections/home2-what-i-do.tsx
+++ src/components/sections/home2-what-i-do.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import { Divider } from "@nextui-org/react";
-import Section from "../ui/section";
-import { Divide } from "lucide-react";
+import Section from "../ui/section-rounded";
+import { ThemeContext } from "../context-providers/theme-context-provider";
 
 const WhatIDo = () => {
+  const theme = useContext(ThemeContext);
   return (
-    <Section color="black">
+    <Section className="bg-[#404040]">
       <div className="container flex justify-between gap-x-6">
-        <div className="flex flex-col w-1/2">
-          <p className="text-white heading">
+        <div className="flex flex-col gap-y-8 w-1/2">
+          <p className="text-white heading leading-tight">
             DREAM. <br /> DRESIGN. <br /> DEVELOP.
           </p>
-          <div className="flex flex-col">
-            <Divider className="bg-white max-w-sm mb-8" />
-            <h2 className="font-semibold text-white mb-6">WHAT I DO</h2>
-            <p className="text-white">
-              Hello! I&apos;m a versatile full stack developer with strong
-              front-end skills and solid back-end expertise. I create intuitive
+          <div className="flex flex-col text-white">
+            <Divider className="bg-black max-w-sm mb-8" />
+            <h2 className="font-semibold mb-6">WHAT I DO</h2>
+            <p>
+              Hello! I&apos;m a versatile full stack web developer with strong
+              front-end expertise and good back-end skills. I create intuitive
               web designs, manage relational and NoSQL databases efficiently,
               and design simple print media. Whether building robust web
               applications or crafting appealing designs, I bring a
@@ -24,10 +26,7 @@ const WhatIDo = () => {
             </p>
           </div>
         </div>
-        <div className="flex flex-col bg-red-500 w-1/2">
-          {" "}
-          Hello jadhsajkdhasjdhkj
-        </div>
+        <div className="flex flex-col w-1/2"></div>
       </div>
     </Section>
   );
--- src/components/sections/home3-featured-project.tsx
+++ src/components/sections/home3-featured-project.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+"use client";
+import React, { useContext } from "react";
 import RedirectLink from "@/components/navigation/redirect-link";
 import Link from "next/link";
 import Image from "next/image";
-import StaticBall from "@/components/global/static-ball";
-import FluidBall from "@/components/global/fluid-ball";
-import Section from "../ui/section";
-import { baiJamjuree } from "@/app/layout";
+import {
+  CyanStaticBall,
+  BlueStaticBall,
+} from "@/components/global/static-ball";
+import Section from "../ui/section-full";
+import { ThemeContext } from "../context-providers/theme-context-provider";
 
 const technologies = [
   "Bootstrap CSS",
@@ -24,8 +27,9 @@ const process = [
 
 const features = ["User login", "Blogs CRUD", "Comments CRUD", "Email"];
 const FeaturedProjects = () => {
+  const theme = useContext(ThemeContext);
   return (
-    <Section className="!relative flex flex-col justify-between" color="black">
+    <Section className="!relative flex flex-col justify-between min-h-screen">
       <Image
         src="/images/projects-featured.png"
         alt="Featured Project - Dreamlabs"
@@ -33,41 +37,51 @@ const FeaturedProjects = () => {
         height={597.03}
         quality={100}
         priority
-        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 !z-20"
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 !z-30"
       />
 
       <div className="container">
         <p
-          className={`text-[56px] font-bold text-center !z-30 text-white ${baiJamjuree.className}`}
+          className={`text-[56px] font-bold text-center !z-30  ${
+            theme === "light" && "text-black"
+          }`}
         >
-          FEATURED <span className="stroked">PROJECT</span>
+          FEATURED PROJECT
         </p>
 
-        <div className="relative flex justify-between">
+        <div className="relative flex justify-between z-20">
           <div className="flex flex-col gap-y-24 max-w-xs min-h-max pb-20">
-            <div className="flex flex-col gap-y-4">
-              <h2 className="font-semibold text-white">Technologies</h2>
+            <div className="flex flex-col gap-y-3">
+              <h2
+                className={`font-semibold ${theme === "light" && "text-black"}`}
+              >
+                Technologies
+              </h2>
               <ul>
                 {technologies.map((tech, i) => (
-                  <li className="text-white" key={i}>
+                  <li className={theme === "light" ? "text-black" : ""} key={i}>
                     {tech}
                   </li>
                 ))}
               </ul>
             </div>
 
-            <div className="flex flex-col gap-y-4">
-              <h2 className="font-semibold text-white">Process</h2>
+            <div className="flex flex-col gap-y-3">
+              <h2
+                className={`font-semibold ${theme === "light" && "text-black"}`}
+              >
+                Process
+              </h2>
               <ul>
                 {process.map((step, i) => (
-                  <li className="text-white" key={i}>
+                  <li className={theme === "light" ? "text-black" : ""} key={i}>
                     {step}
                   </li>
                 ))}
               </ul>
             </div>
 
-            <p className="text-white">
+            <p className={theme === "light" ? "text-black" : ""}>
               Tutorial:{" "}
               <Link
                 href="https://youtu.be/vCOSTG10Y4o?si=rDJaiTIYPRlsjXdd"
@@ -78,28 +92,39 @@ const FeaturedProjects = () => {
               </Link>
             </p>
           </div>
-          <div className="absolute top-[45%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 !z-10">
-            <StaticBall />
+
+          <div className="absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 !z-10">
+            <BlueStaticBall />
           </div>
 
           <div className="flex flex-col justify-between items-end max-w-xs min-h-max text-end pb-20">
-            <div className="flex flex-col gap-y-4">
-              <h2 className="font-semibold text-white">Features</h2>
+            <div className="flex flex-col gap-y-3">
+              <h2
+                className={`font-semibold ${theme === "light" && "text-black"}`}
+              >
+                Features
+              </h2>
               <ul>
                 {features.map((feature, i) => (
-                  <li className="text-white" key={i}>
+                  <li className={theme === "light" ? "text-black" : ""} key={i}>
                     {feature}
                   </li>
                 ))}
               </ul>
             </div>
 
-            <div className="flex flex-col gap-y-4">
-              <h2 className="font-semibold text-white">Visit here</h2>
+            <div className={`flex flex-col gap-y-3`}>
+              <h2
+                className={`font-semibold ${theme === "light" && "text-black"}`}
+              >
+                Visit here
+              </h2>
               <Link
                 href="https://dreamlabs.vercel.app/"
                 target="_blank"
-                className="underline underline-offset-2 externalLink font-light text-white"
+                className={`underline underline-offset-2 externalLink ${
+                  theme === "light" ? "text-black" : "text-white"
+                }`}
               >
                 dreamlabs.vercel.app
               </Link>
