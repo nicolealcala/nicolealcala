44507c0c0ec1de3b5234a499403c911dedcccd8f
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-09 17:20:42 +0800

    added route for specific note

---
+++ src/app/api/magic-shop/[slug]/route.ts
@@ -0,0 +1,23 @@
+import connectToDB from "@/config/db";
+import { Note } from "@/lib/models";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest, { params }: any) => {
+  const { slug } = params;
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
+  try {
+    await connectToDB();
+    if (code && code !== "") {
+      const note = await Note.findOne({ code });
+      return NextResponse.json(note, { status: 200 });
+    }
+
+    const notes = await Note.find({
+      noteFor: { $regex: new RegExp(slug, "i") },
+    });
+    return NextResponse.json(notes, { status: 200 });
+  } catch (error) {
+    console.log(error);
+  }
+};
