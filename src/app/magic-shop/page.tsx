48ef0d934c351aa22a04e895c6d4df2bd1c0b232
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-09 17:21:08 +0800

    created a separate component for note

--- src/app/magic-shop/page.tsx
+++ src/app/magic-shop/page.tsx
@@ -1,11 +1,10 @@
 "use client";
-import NoteHoles from "@/components/magic-shop/note-holes";
 import Section from "@/components/ui/section-full";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
 import SecretCode from "@/components/magic-shop/secret-code";
-import NoteSkeleton from "@/components/magic-shop/note-skeleton";
+import Note from "@/components/magic-shop/note";
 
 interface NoteData {
   _id: string;
@@ -40,44 +39,10 @@ const MagicShop = () => {
     <main>
       <Section>
         <div className="container flex flex-col items-center min-h-[80vh] gap-y-10">
-          <h1 className="font-bold text-4xl text-center">
+          <h1 className="font-bold text-4xl text-center text-white">
             A sneak peak at my notes app...
           </h1>
-          {code ? (
-            <div className="flex flex-col border items-center justify-center border-white max-w-2xl p-4 rounded-lg bg-[#303030] z-50">
-              <NoteHoles />
-              {data ? (
-                <div className="flex flex-col gap-y-4 p-4 mt-4">
-                  <p className="flex justify-end text-xs text-yellow">
-                    Written on {data?.writtenOn}
-                  </p>
-                  <p className="mt-6 font-bold italic underline underline-offset-[12px] decoration-yellow">
-                    To {data?.noteFor},
-                  </p>
-                  <div className="letter">
-                    {data?.note.map((content, index: any) => (
-                      <p key={index} className="indent-8 leading-7 mt-6">
-                        {content}
-                      </p>
-                    ))}
-                  </div>
-                  <p className="mt-6 font-bold italic text-right">
-                    <span className="font-normal">Always here,</span>
-                    <br />
-                    Nicole
-                  </p>
-                  <p className="text-xs mt-6 text-gray-500">
-                    This note will be removed in 3 days. Scan the QR code for
-                    daily bible verses.
-                  </p>
-                </div>
-              ) : (
-                <NoteSkeleton />
-              )}
-            </div>
-          ) : (
-            <SecretCode />
-          )}
+          {code ? <Note data={data} /> : <SecretCode />}
         </div>
       </Section>
     </main>
---
+++ src/components/magic-shop/note.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import NoteHoles from "./note-holes";
+import NoteSkeleton from "./note-skeleton";
+
+const Note = ({ data }: any) => {
+  return (
+    <div className="flex flex-col gap-y-10">
+      <h1 className="font-bold text-4xl text-center text-white">
+        A sneak peak at my notes app...
+      </h1>
+      <div className="flex flex-col border items-center justify-center border-white max-w-2xl p-4 rounded-lg bg-[#303030] z-50">
+        <NoteHoles />
+        {data ? (
+          <div className="flex flex-col gap-y-4 p-4 mt-4 text-white">
+            <p className="flex justify-end text-xs text-yellow">
+              Written on {data?.writtenOn}
+            </p>
+            <p className="mt-6 font-bold italic underline underline-offset-[12px] decoration-yellow">
+              To {data?.noteFor},
+            </p>
+            <div className="letter">
+              {data?.note.map((content: string, index: any) => (
+                <p key={index} className="indent-8 leading-7 mt-6">
+                  {content}
+                </p>
+              ))}
+            </div>
+            <p className="mt-6 font-bold italic text-right">
+              <span className="font-normal">Always here,</span>
+              <br />
+              Nicole
+            </p>
+            <p className="text-xs mt-6 text-gray-500">
+              This note will be removed in 3 days. Scan the QR code for daily
+              bible verses.
+            </p>
+          </div>
+        ) : (
+          <NoteSkeleton />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Note;
