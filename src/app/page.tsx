86a6b2bc07a8eb340f2ce6dfb04ccccc6337e82f
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-15 11:35:59 +0800

    code cleanup, removed manual theme configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-
 import Landing from "@/components/sections/home1-landing";
-
 import FeaturedProjects from "@/components/sections/home3-featured-project";
 import WhatIDo from "@/components/sections/home2-what-i-do";
 import Testimonials from "@/components/sections/home4-testimonials";
--- src/components/sections/home2-what-i-do.tsx
+++ src/components/sections/home2-what-i-do.tsx
@@ -7,30 +7,16 @@ import { GlobalContext } from "../context-providers/global-context-provider";
 const WhatIDo = () => {
   const { theme } = useContext(GlobalContext);
   return (
-    <Section className="bg-[#404040]">
+    <Section className="highlighted-div">
       <div className="container flex justify-between gap-x-6">
         <div className="flex flex-col gap-y-8 w-1/2">
-          <p
-            className={`heading leading-tight ${
-              theme === "light" && "text-black"
-            }`}
-          >
+          <p className="heading leading-tight">
             DREAM. <br /> DRESIGN. <br /> DEVELOP.
           </p>
           <div className="flex flex-col">
-            <Divider
-              className={`max-w-sm mb-8 ${
-                theme === "dark" ? "bg-white" : "bg-black"
-              }`}
-            />
-            <h2
-              className={`font-semibold mb-6 ${
-                theme === "light" && "text-black"
-              }`}
-            >
-              WHAT I DO
-            </h2>
-            <p className={theme === "light" ? "text-black" : ""}>
+            <Divider className="max-w-sm mb-8 divider" />
+            <h2 className="font-semibold mb-6">WHAT I DO</h2>
+            <p>
               Hello! I&apos;m a versatile full stack web developer with strong
               front-end expertise and good back-end skills. I create intuitive
               web designs, manage relational and NoSQL databases efficiently,
