04ba8e43c8ebcf64970e78d88bdf26f4c216889c
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-28 18:15:20 +0800

    responsiveness

--- src/components/sections/home2-what-i-do.tsx
+++ src/components/sections/home2-what-i-do.tsx
@@ -7,17 +7,17 @@ import { GlobalContext } from "../context-providers/global-context-provider";
 const WhatIDo = () => {
   const { theme } = useContext(GlobalContext);
   return (
-    <Section className="highlighted-div">
+    <Section className="bg-black">
       <div className="container flex flex-col justify-between  lg:flex-row gap-6">
         <div className="flex flex-col gap-y-8 w-full lg:w-1/2">
-          <p className="heading leading-tight">
-            DREAM. <br /> DRESIGN. <br /> DEVELOP.
+          <p className="heading leading-tight text-white">
+            DREAM. <br /> DESIGN. <br /> DEVELOP.
           </p>
-          <div className="flex flex-col">
-            <Divider className="max-w-sm mb-8 divider" />
+          <div className="flex flex-col text-white">
+            <Divider className="max-w-sm mb-8 bg-white" />
             <h2 className="font-semibold mb-6">WHAT I DO</h2>
             <p>
-              Hello! I&apos;m a versatile full stack web developer with strong
+              Hello! I&apos;m a versatile full-stack web developer with strong
               front-end expertise and good back-end skills. I create intuitive
               web designs, manage relational and NoSQL databases efficiently,
               and design simple print media. Whether building robust web
--- src/components/sections/home3-featured-project.tsx
+++ src/components/sections/home3-featured-project.tsx
@@ -14,12 +14,14 @@ const technologies = [
   "NextAuth.js",
   "MongoDB",
   "Mongoose",
+  "NodeMailer",
 ];
 
 const process = [
   "Followed a 4-hour YouTube tutorial.",
   "Edited the styles and assets.",
   "Added commenting functionality.",
+  "Added email functionality.",
 ];
 
 const features = ["User login", "Blogs CRUD", "Comments CRUD", "Email"];
@@ -27,7 +29,7 @@ const features = ["User login", "Blogs CRUD", "Comments CRUD", "Email"];
 const FeaturedProjects = () => {
   const { theme } = useContext(GlobalContext);
   return (
-    <Section className="!relative flex flex-col justify-between min-h-screen">
+    <Section className="!relative flex flex-col justify-between pb-0 md:pb-24">
       <Image
         src="/images/projects-featured.png"
         alt="Featured Project - Dreamlabs"
@@ -35,16 +37,22 @@ const FeaturedProjects = () => {
         height={597.03}
         quality={100}
         priority
-        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 !z-30"
+        className="hidden md:flex absolute bottom-0 left-1/2 transform -translate-x-1/2 !z-30"
       />
 
-      <div className="container">
-        <p className="text-[56px] font-bold text-center !z-30">
-          FEATURED PROJECT
-        </p>
-
-        <div className="relative flex justify-between z-20">
-          <div className="flex flex-col gap-y-16 max-w-xs min-h-max pb-20">
+      <div className="container flex flex-col">
+        <p className="subheading font-bold text-center">FEATURED PROJECT</p>
+        <Image
+          src="/images/projects-featured-mobile.png"
+          alt="Featured Project - Dreamlabs"
+          width={600}
+          height={750}
+          quality={100}
+          priority
+          className="flex md:hidden"
+        />
+        <div className="hidden md:flex flex-col gap-6 md:gap-12 pb-8 relative">
+          <div className="flex justify-between">
             <div className="flex flex-col gap-y-3 z-10">
               <h2 className="font-semibold">Technologies</h2>
               <ul>
@@ -55,7 +63,19 @@ const FeaturedProjects = () => {
                 ))}
               </ul>
             </div>
+            <div className="flex flex-col gap-y-3 text-right">
+              <h2 className="font-semibold">Features</h2>
+              <ul>
+                {features.map((feature, i) => (
+                  <li key={i} className="text-sm">
+                    {feature}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
 
+          <div className="flex justify-between gap-6">
             <div className="flex flex-col gap-y-3">
               <h2 className="font-semibold">Process</h2>
               <ul>
@@ -66,9 +86,23 @@ const FeaturedProjects = () => {
                 ))}
               </ul>
             </div>
+            <div className="w-5 h-16 bg-yellow-200 place-self-end"></div>
+          </div>
 
+          <div className="flex justify-between items-end">
             <div className="w-16 h-5 bg-yellow-200"></div>
-
+            <div className="flex flex-col gap-y-3 text-right">
+              <h2 className="font-semibold">Visit here</h2>
+              <Link
+                href="https://dreamlabs.vercel.app/"
+                target="_blank"
+                className="underline underline-offset-2 transition-colors duration-300 text-sm"
+              >
+                dreamlabs.vercel.app
+              </Link>
+            </div>
+          </div>
+          <div className="flex justify-between">
             <p className="text-sm">
               Tutorial:{" "}
               <Link
@@ -79,38 +113,11 @@ const FeaturedProjects = () => {
                 Next.js Full Course Tutorial
               </Link>
             </p>
-          </div>
-
-          <div className="absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 !z-10">
-            {theme === "light" ? <BlueStaticBall /> : <CyanStaticBall />}
-          </div>
-
-          <div className="flex flex-col justify-between items-end max-w-xs min-h-max text-end pb-20">
-            <div className="flex flex-col gap-y-3">
-              <h2 className="font-semibold">Features</h2>
-              <ul>
-                {features.map((feature, i) => (
-                  <li key={i} className="text-sm">
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="w-5 h-16 bg-yellow-200"></div>
-
-            <div className="flex flex-col gap-y-3">
-              <h2 className="font-semibold">Visit here</h2>
-              <Link
-                href="https://dreamlabs.vercel.app/"
-                target="_blank"
-                className="underline underline-offset-2 transition-colors duration-300 text-sm"
-              >
-                dreamlabs.vercel.app
-              </Link>
-            </div>
-
             <RedirectLink path="/projects" text="my portfolio" />
+          </div>
+
+          <div className="hidden md:flex absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 !z-10">
+            {theme === "light" ? <BlueStaticBall /> : <CyanStaticBall />}
           </div>
         </div>
       </div>
