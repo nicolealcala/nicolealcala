ca4701d095f990adf394ae41ff26917c80968592
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-28 18:42:54 +0800

    added id for last section

--- src/components/ui/section-full.tsx
+++ src/components/ui/section-full.tsx
@@ -5,10 +5,16 @@ interface SectionProps {
   className?: string;
   [key: string]: any;
 }
-const Section: React.FC<SectionProps> = ({ children, className, ...props }) => {
+const Section: React.FC<SectionProps> = ({
+  children,
+  className,
+  id,
+  ...props
+}) => {
   return (
     <section
       className={`px-6 lg:px-24 py-16 flex justify-center items-center ${className}`}
+      id={id}
       {...props}
     >
       {children}
