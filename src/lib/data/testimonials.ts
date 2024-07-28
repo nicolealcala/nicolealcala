d9c01463fe7dc0c1270f6255361f4ff6e5f5922e
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-28 16:28:14 +0800

    testimonials data

---
+++ src/lib/data/testimonials.ts
@@ -0,0 +1,36 @@
+const Feedback = [
+  {
+    name: "CJ De Claro",
+    role: "Software Engineer, Professor",
+    feedback:
+      "Nicole will never settle for less. She embodies what a PUPian is: with high standard. She has a professional aptitude and she is a great asset to any organization that she is, or will be, a part of.",
+    color: "blue",
+    img: "/images/testimonials/cj.jpg",
+  },
+  {
+    name: "Patricia Abrigo",
+    role: "UI/UX Designer",
+    feedback:
+      "I have worked with Nicole on several projects, and her work ethic is noteworthy. Her attention to detail, ability to collaborate well, and passion for learning led us to successful completion of projects. With that, Nicole's reliability will surely make meaningful contributions in her professional endeavors.",
+    color: "pink",
+    img: "/images/testimonials/user.png",
+  },
+  {
+    name: "JM Morano",
+    role: "Web Graphics Designer",
+    feedback:
+      "I've had the privilege of working with Nicole for several years, and I can confidently say that her dedication to her work is truly exceptional. We were frequently put in the same group throughout our time at the university and even served as fellow interns at The BLOKC. She pays excellent attention to detail and meticulously examines the output for any bugs or issues, ensuring that everything works as intended. Her high standards, whether as a developer or a designer, are widely observed with the quality of her work. She does not settle for mediocre or average output, always striving for the best possible results.",
+    color: "blue",
+    img: "/images/testimonials/jm.jpg",
+  },
+  {
+    name: "Danna Layderos",
+    role: "UI/UX Designer",
+    feedback:
+      "Nicole is an exceptionally talented individual, proficient in coding and adept at UI/UX design. Her keen intuition allows her to take initiative and solve problems independently, showcasing her cool, self-reliant approach. Moreover, her creativity shines through not only in her technical skills but also in her musical talents, where she exhibits both proficiency in various instruments and a truly captivating voice. Although her artistry may not be as frequent, it is undeniably impressive when she does create.",
+    color: "pink",
+    img: "/images/testimonials/user.png",
+  },
+];
+
+export default Feedback;
