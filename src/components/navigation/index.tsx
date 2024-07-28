e03160e22a6e03632ff783b156081b501f285f6e
Nicole Alcala <nicolealcalq@gmail.com>
2024-07-28 21:14:55 +0800

    added collapsed nav for small screens

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -2,9 +2,10 @@
 import Image from "next/image";
 import { Link, Button } from "@nextui-org/react";
 import { usePathname } from "next/navigation";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../context-providers/global-context-provider";
-import { Sun1, Moon } from "iconsax-react";
+// import { Sun1, Moon } from "iconsax-react";
+import "./nav.scss";
 
 const links = [
   { path: "/", title: "Home" },
@@ -13,26 +14,28 @@ const links = [
 ];
 
 const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
   const { theme, setTheme, handleThemeChange } = useContext(GlobalContext);
 
   return (
     <nav
-      className={`bg-transparent py-5 px-20 flex justify-between top-0 !z-50 transition-colors duration-500 w-full ${
+      className={`bg-transparent py-5 px-4 md:px-10 flex flex-col gap-3 top-0 !z-50 w-full ${
         theme === "dark" && "text-white"
       }`}
       id="navbar"
     >
-      <div className="flex gap-x-3">
-        <Image
-          src="/images/logo.png"
-          alt="Logo"
-          width={38.25}
-          height={32}
-          priority
-        />
-        <h2 className="font-semibold leading-9">clinoae</h2>
-        <Button
+      <div className="flex justify-between items-center w-full">
+        <div className="flex gap-x-3">
+          <Image
+            src="/images/logo.png"
+            alt="Logo"
+            width={38.25}
+            height={32}
+            priority
+          />
+          <h2 className="font-semibold leading-9">clinoae</h2>
+          {/* <Button
           isIconOnly
           className="bg-transparent"
           disableAnimation
@@ -53,34 +56,77 @@ const Nav = () => {
               className=" hover:opacity-75 transition-all duration-100 ease-in"
             />
           )}
-        </Button>
+        </Button> */}
+        </div>
+
+        {/* Collapsed nav button*/}
+        <label className="burger z-30" htmlFor="burger">
+          <input
+            type="checkbox"
+            id="burger"
+            checked={isOpen}
+            onChange={() => setIsOpen((prev) => !prev)}
+          />
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+
+        <div className="hidden md:flex gap-x-3 items-center">
+          {links.map((link, index) => (
+            <Link
+              href={link.path}
+              key={index}
+              className={`transition-colors duration-500 ${
+                theme === "dark" ? "text-white" : "text-black"
+              }`}
+            >
+              <span
+                className={`font-semibold py-2 px-3 text-center ${
+                  link.path === pathname && "active"
+                }`}
+              >
+                {link.title}
+              </span>
+            </Link>
+          ))}
+          <Button
+            as={Link}
+            href="/contact"
+            variant={pathname === "/contact" ? "solid" : "bordered"}
+            className={`rounded border-2 ${
+              pathname === "/contact"
+                ? "bg-white text-black border-black"
+                : "bg-black text-white  border-white"
+            }`}
+          >
+            <span className="font-semibold text-base">Contact Me</span>
+          </Button>
+        </div>
       </div>
-      <div className="flex gap-x-10 items-center">
+      {/* Collapssed nav items */}
+      <div
+        className={`md:hidden flex-col top-0 right-0 w-full transition-height duration-300 ease-linear ${
+          isOpen ? "h-full flex " : "h-0 hidden"
+        }`}
+      >
         {links.map((link, index) => (
           <Link
             href={link.path}
             key={index}
-            className={`transition-colors duration-500 ${
+            className={`transition-colors duration-500 max-w-max ${
               theme === "dark" ? "text-white" : "text-black"
             }`}
           >
             <span
-              className={`font-semibold ${link.path === pathname && "active"}`}
+              className={`font-semibold py-2 px-3 text-center ${
+                link.path === pathname && "active"
+              }`}
             >
               {link.title}
             </span>
           </Link>
         ))}
-        <Button
-          as={Link}
-          href="/contact"
-          variant={pathname === "/contact" ? "solid" : "bordered"}
-          className={`rounded ${
-            theme === "dark" ? "text-white" : " border-black text-black"
-          }`}
-        >
-          <span className="font-semibold text-base">Contact Me</span>
-        </Button>
       </div>
     </nav>
   );
