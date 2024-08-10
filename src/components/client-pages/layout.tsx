3d7133951f8353309c045e947d1e8b81d162a70e
Nicole Alcala <nicolealcalq@gmail.com>
2024-08-10 16:22:59 +0800

    conditionally render footer

--- src/components/client-pages/layout.tsx
+++ src/components/client-pages/layout.tsx
@@ -3,15 +3,17 @@ import { useContext } from "react";
 import Navbar from "../navigation";
 import Footer from "../ui/footer";
 import { GlobalContext } from "../context-providers/global-context-provider";
+import { usePathname } from "next/navigation";
 
 const ClientLayout: React.FC<any> = ({ children }) => {
   const { theme } = useContext(GlobalContext);
+  const pathname = usePathname();
   return (
     <div className={theme.toString()}>
       <Navbar />
       <div className="!z-10">
         {children}
-        <Footer />
+        {pathname === "/magic-shop" ? null : <Footer />}
       </div>
     </div>
   );
