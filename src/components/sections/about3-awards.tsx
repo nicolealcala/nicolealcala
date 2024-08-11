484f07ede5bcce8a5c3b5f1f84a24e279fe73c07
Nicole Alcala <nicolealcalq@gmail.com>
2024-08-11 19:09:00 +0800

    completed ui for awards section

--- src/components/sections/about3-awards.tsx
+++ src/components/sections/about3-awards.tsx
@@ -1,24 +1,35 @@
 import React from "react";
 import Section from "../ui/section-full";
 import awards from "@/lib/data/awards";
-import Image from "next/image";
+import { ChevronsRight } from "lucide-react";
 import "./sections.scss";
+import Link from "next/link";
 
 const Awards = () => {
   return (
     <Section className="bg-black">
       <div className="container text-white flex flex-col gap-y-12">
         <h1 className="text-pink font-semibold">Awards & Recognitions</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-6 w-full">
           {awards.map((award, i) => (
             <div
               key={i}
               className="relative bg-yellow-900 w-full h-[350px] max-w-[400px] max-h-[350px] place-self-center rounded overflow-hidden"
             >
               <div className="overlay absolute top-0 left-0 w-full h-[350px] max-w-[400px] max-h-[350px] z-20 opacity-0 hover:opacity-100 transition duration-300 ease-in-out flex flex-col gap-y-1 items-center justify-center">
-                <h2 className="font-semibold">{award.title}</h2>
+                <h2 className="text-center font-semibold">{award.title}</h2>
                 <p className="text-center mt-2">{award.description}</p>
-                <p className="text-sm italic">{award.date}</p>
+                <div className="flex gap-x-2 items-center">
+                  <p className="text-center text-sm italic">{award.date}</p> |
+                  {award.link && (
+                    <Link
+                      href={award.link}
+                      className="flex gap-x-1 items-center text-sm italic"
+                    >
+                      View official list <ChevronsRight />
+                    </Link>
+                  )}
+                </div>
               </div>
               <img
                 src={award.image}
