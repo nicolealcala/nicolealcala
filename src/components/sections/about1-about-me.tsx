0e3c95b13418e12e28c55e16e1c7850c546a4adf
Nicole Alcala <nicolealcalq@gmail.com>
2024-08-17 20:52:10 +0800

    updated white texts

--- src/components/sections/about1-about-me.tsx
+++ src/components/sections/about1-about-me.tsx
@@ -7,16 +7,16 @@ import "./sections.scss";
 const AboutMe = () => {
   return (
     <Section className="bg-black">
-      <div className="container flex flex-col gap-y-12 text-white">
-        <p className="title sm:text-8xl md:text-9xl font-medium w-full text-center">
+      <div className="container flex flex-col gap-y-12">
+        <p className="title sm:text-8xl md:text-9xl font-medium w-full text-center text-white">
           ABOUT ME
         </p>
         <div className="flex flex-1 justify-between">
           <div className="flex flex-col gap-y-24">
             {/* EDUCATION */}
             <div className="flex flex-col gap-y-4 max-w-[375px]">
-              <h2 className="font-semibold">EDUCATION</h2>
-              <p>
+              <h2 className="font-semibold text-white">EDUCATION</h2>
+              <p className="text-white">
                 BS in Information Technology
                 <br />
                 Polytechnic University of the Philippines
@@ -27,8 +27,8 @@ const AboutMe = () => {
 
             {/* EXPERIENCE */}
             <div className="flex flex-col gap-y-4 max-w-[375px]">
-              <h2 className="font-semibold">EXPERIENCE</h2>
-              <p>
+              <h2 className="font-semibold text-white">EXPERIENCE</h2>
+              <p className="text-white">
                 Analyst Programmer |{" "}
                 <span className="text-yellow">Full-stack Web Developer</span>
                 <br />
@@ -43,14 +43,15 @@ const AboutMe = () => {
               path="https://drive.google.com/file/d/1SIESrpCjLwpHLKTz7xqpOfgynaI8d0cL/view?usp=sharing"
               text="view my cv"
               target="_blank"
+              className="text-white"
             />
           </div>
 
           <div className="flex flex-col justify-between text-right gap-y-16">
             {/* FACTS */}
             <div className="flex flex-col gap-y-4 max-w-[375px]">
-              <h2 className="font-semibold">FACTS</h2>
-              <p>
+              <h2 className="font-semibold text-white">FACTS</h2>
+              <p className="text-white">
                 <span className="text-2xl font-bold text-yellow">
                   INTROVERTED
                 </span>
