c2aca5151fa5243cab842409c3d9851e23818a56
Nicole Alcala <nicolealcalq@gmail.com>
2024-08-18 17:04:11 +0800

    replaced color black with slate-700 hex

--- src/components/icons/facebook.tsx
+++ src/components/icons/facebook.tsx
@@ -17,14 +17,16 @@ const Facebook = ({ placement }: any) => {
       >
         <path
           d="M13.2 12.9H14.7L15.3 10.5H13.2V9.3C13.2 8.682 13.2 8.1 14.4 8.1H15.3V6.084C15.1044 6.0582 14.3658 6 13.5858 6C11.9568 6 10.8 6.9942 10.8 8.82V10.5H9V12.9H10.8V18H13.2V12.9Z"
-          fill={theme === "dark" || placement === "footer" ? "white" : "black"}
+          fill={
+            theme === "dark" || placement === "footer" ? "white" : "#334155"
+          }
         />
         <circle
           cx="12"
           cy="12"
           r="11.5"
           stroke={
-            theme === "dark" || placement === "footer" ? "white" : "black"
+            theme === "dark" || placement === "footer" ? "white" : "#334155"
           }
         />
       </svg>
--- src/components/icons/x.tsx
+++ src/components/icons/x.tsx
@@ -17,14 +17,16 @@ const X = ({ placement }: any) => {
       >
         <path
           d="M15.4272 6H17.4578L13.0221 11.0826L18.2409 18H14.1551L10.9527 13.8055L7.29256 18H5.26009L10.0041 12.5618L5 6.00095H9.18979L12.0801 9.83417L15.4272 6ZM14.7131 16.7818H15.8386L8.57503 7.15479H7.36822L14.7131 16.7818Z"
-          fill={theme === "dark" || placement === "footer" ? "white" : "black"}
+          fill={
+            theme === "dark" || placement === "footer" ? "white" : "#334155"
+          }
         />
         <circle
           cx="12"
           cy="12"
           r="11.5"
           stroke={
-            theme === "dark" || placement === "footer" ? "white" : "black"
+            theme === "dark" || placement === "footer" ? "white" : "#334155"
           }
         />
       </svg>
