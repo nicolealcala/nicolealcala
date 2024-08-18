86314ae510842277271b5c1c5e7ab3d2ba0d43e1
Nicole Alcala <nicolealcalq@gmail.com>
2024-08-18 14:52:52 +0800

    updted colors and user image

--- src/components/sections/home4-testimonials.tsx
+++ src/components/sections/home4-testimonials.tsx
@@ -5,17 +5,17 @@ import Feedback from "@/lib/data/testimonials";
 
 const Testimonials = () => {
   return (
-    <Section className="bg-black" id="last">
+    <Section className="bg-powder-blue-500" id="last">
       <div className="container flex flex-col items-center text-white gap-y-12">
         <div className="flex flex-col gap-y-6 items-center ">
           <p className="text-3xl text-white md:text-5xl font-bold text-center">
-            <span className="text-pink">Excellence</span> is a continuous
-            journey
+            <span className="text-soft-pink-200">Excellence</span> is a
+            continuous journey
           </p>
           <p className="text-white text-center max-w-[90%] md:max-w-[60%]">
-            and your <span className="highlighted-text">feedback</span> is the
-            fuel that keeps me striving for it. It empowers me to embrace
-            challenges, learn from experiences, and continuously raise the bar.
+            and your feedback is the fuel that keeps me striving for it. It
+            empowers me to embrace challenges, learn from experiences, and
+            continuously raise the bar.
           </p>
         </div>
         <EmblaCarousel items={Feedback} />
