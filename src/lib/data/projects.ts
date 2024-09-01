2a4b5162687f35845806a7bd20cb97b895915f9f
Nicole Alcala <nicolealcalq@gmail.com>
2024-09-01 17:31:30 +0800

    awards and projects file

---
+++ src/lib/data/projects.ts
@@ -0,0 +1,128 @@
+const projects = [
+  {
+    title: "Dreamlabs",
+    details: "Demo Project",
+    period: "April - May 2024",
+    techStack: ["Next.js", "CSS", "Bootstrap", "MongoDB", "Mongoose"],
+    links: [
+      { github: "https://github.com/nicolealcala/dreamlabs" },
+      { hosted: "https://dreamlabs.vercel.app/" },
+    ],
+    category: ["development"],
+    image: "/images/projects/dreamlabs/thumbnail.png",
+  },
+  {
+    title: "Graduation Stationery",
+    details: "PUP-STC 28th Commencement Exercises",
+    period: "September 2023",
+    techStack: [],
+    links: [],
+    category: ["design"],
+    image: "/images/projects/graduation-stationery/thumbnail.png",
+  },
+  {
+    title: "Nicole Alcala",
+    details: "Portfolio Website",
+    period: "August - October 2023",
+    techStack: ["HTML", "CSS", "Bootstrap", "JavaScript/jQuery"],
+    links: [
+      {
+        figma:
+          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?t=nHS6V4U1OezvHq5g-0",
+      },
+      { hosted: "https://nicolealcala.github.io/" },
+    ],
+    category: ["development", "design"],
+    image: "/images/projects/portfolio-v1/thumbnail.png",
+  },
+  {
+    title: "Medeco",
+    details: "Demo Mobile App Design",
+    period: "September 2023",
+    techStack: [],
+    links: [
+      {
+        figma:
+          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=0-1&node-type=CANVAS&t=FJEsCrT1GI5BDUJG-0",
+      },
+    ],
+    category: ["design"],
+    image: "/images/projects/medeco/thumbnail.png",
+  },
+  {
+    title: "UNBLOKC Hackathon Wristbands",
+    details: "Internship Project",
+    period: "June 2023",
+    techStack: [],
+    links: [
+      {
+        figma:
+          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=936-358&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+      },
+    ],
+    category: ["design"],
+    image: "/images/projects/unblokc-website/thumbnail.png",
+  },
+  {
+    title: "UNBLOKC Hackathon",
+    details: "Internship Project",
+    period: "April - May 2023",
+    techStack: ["HTML", "CSS", "JavaScript/jQuery"],
+    links: [
+      {
+        figma:
+          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2271&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+      },
+      { hosted: "https://hackathon.theblokc.media/" },
+    ],
+    category: ["development", "design"],
+    image: "/images/projects/unblokc-website/thumbnail.png",
+  },
+  {
+    title: "Katipunan ng Kabataan Information System (KKIS)",
+    details: "Capstone Project",
+    period: "August 2022 - February 2023",
+    techStack: ["PHP", "Sass", "Bootstrap", "JavaScript/jQuery", "MySQL"],
+    links: [
+      { github: "https://github.com/nicolealcala/KKIS" },
+      {
+        figma:
+          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=1-1292&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=1-1292&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+      },
+      { hosted: "https://kkinfosystem.000webhostapp.com/" },
+    ],
+    category: ["development", "design"],
+    image: "/images/projects/kkis/thumbnail.png",
+  },
+  {
+    title: "Loungescape",
+    details: "School Project",
+    period: "February - March 2022",
+    techStack: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    links: [
+      { github: "https://github.com/markprongoso/LoungEscape" },
+      {
+        figma:
+          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2274&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+      },
+    ],
+    category: ["development", "design"],
+    image: "/images/projects/loungescape/thumbnail.png",
+  },
+  {
+    title: "Strays Worth Saving",
+    details: "Demo Web Design",
+    period: "December 2021",
+    techStack: [],
+    links: [
+      {
+        figma:
+          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2272&node-type=CANVAS&t=UWuuJGTvPIzWm04z-0",
+      },
+    ],
+    category: ["design"],
+    image: "/images/projects/sws/thumbnail.png",
+  },
+];
+
+export default projects;
