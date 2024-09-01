5e95e6db9114993b442326eadf2010fa4e18e1ab
Nicole Alcala <nicolealcalq@gmail.com>
2024-09-01 17:30:53 +0800

    moved awards variable in separate lib file

--- src/components/sections/about3-awards.tsx
+++ src/components/sections/about3-awards.tsx
@@ -4,68 +4,18 @@ import { ChevronsRight } from "lucide-react";
 import "./sections.scss";
 import Link from "next/link";
 import Image from "next/image";
-
-const awards = [
-  {
-    title: "WPH Code Bootcamp",
-    description: "WPH Digital Pte Ltd",
-    date: "December 2023",
-    image: "/images/awards/wph-bootcamp.png",
-  },
-  {
-    title: "Magna Cum Laude",
-    description:
-      "Polytechnic University of the Philippines - Sto. Tomas Campus",
-    date: "Class of 2023",
-    image: "/images/awards/pup-mcl.jpg",
-  },
-  {
-    title: "DOST Academic Excellence Award",
-    description: "Department of Science & Technology - CALABARZON",
-    date: "October 2023",
-    image: "/images/awards/dost-excellence.png",
-  },
-  {
-    title: "Academic Achiever, Rank 1 (BSIT)",
-    description:
-      "Polytechnic University of the Philippines - Sto. Tomas Campus",
-    date: "A.Y. 2022-2023",
-    image: "/images/awards/pup-r1.png",
-  },
-  {
-    title: "President's List, 8 semesters",
-    description:
-      "Polytechnic University of the Philippines - Sto. Tomas Campus",
-    date: "2019-2023",
-    image: "/images/awards/pup-pl.jpg",
-  },
-  {
-    title: "Academic Achiever, Rank 2 (BSIT)",
-    description:
-      "Polytechnic University of the Philippines - Sto. Tomas Campus",
-    date: "A.Y. 2020-2021",
-    image: "/images/awards/pup-r2.png",
-  },
-  {
-    title: "S&T Undergraduate Scholarship, RA 7687",
-    description:
-      "Department of Science & Technology - Science Eduation Institute",
-    date: "2019-2023",
-    image: "/images/awards/dost-scholarship.png",
-    link: "https://sei.dost.gov.ph/wp-content/uploads/2024/01/2019DOSTSEIQualifiers.pdf",
-  },
-];
+import awards from "@/lib/data/awards";
 
 const Awards = () => {
   return (
     <Section className="bg-black">
-      <div className="container text-white flex flex-col gap-y-12">
+      <div className="container text-white flex flex-col gap-y-12 w-full">
         <h1 className="text-pink-400 font-semibold">Awards & Recognitions</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-6 w-full">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-6">
           {awards.map((award, i) => (
             <div
               key={i}
-              className="relative bg-yellow-900 w-full h-[350px] max-w-[400px] max-h-[350px] place-self-center rounded overflow-hidden"
+              className="relative w-full h-[350px] max-w-[400px] max-h-[350px] place-self-center rounded overflow-hidden"
             >
               <div className="overlay p-6 absolute top-0 left-0 w-full h-[350px] max-w-[400px] max-h-[350px] z-20 opacity-0 hover:opacity-100 transition duration-300 ease-in-out flex flex-col gap-y-1 items-center justify-center">
                 <h2 className="text-center font-semibold">{award.title}</h2>
