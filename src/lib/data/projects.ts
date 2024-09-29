6d700121b1efd7d5048240c4253d5b637ab37f65
Nicole Alcala <nicolealcalq@gmail.com>
2024-09-29 14:59:25 +0800

    mpdified links attribute from array to object

--- src/lib/data/projects.ts
+++ src/lib/data/projects.ts
@@ -4,10 +4,11 @@ const projects = [
     details: "Demo Project",
     period: "April - May 2024",
     techStack: ["Next.js", "CSS", "Bootstrap", "MongoDB", "Mongoose"],
-    links: [
-      { github: "https://github.com/nicolealcala/dreamlabs" },
-      { hosted: "https://dreamlabs.vercel.app/" },
-    ],
+    links: {
+      github: "https://github.com/nicolealcala/dreamlabs",
+      hosted: "https://dreamlabs.vercel.app/",
+    },
+
     category: ["development"],
     image: "/images/projects/dreamlabs/thumbnail.png",
   },
@@ -16,12 +17,11 @@ const projects = [
     details: "Demo Web Design",
     period: "October 2023",
     techStack: [],
-    links: [
-      {
-        figma:
-          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2272&node-type=CANVAS&t=UWuuJGTvPIzWm04z-0",
-      },
-    ],
+    links: {
+      figma:
+        "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=966-2&node-type=canvas&t=MWSbTMAW6ggkLXaf-0",
+    },
+
     category: ["design"],
     image: "/images/projects/pawhub/thumbnail.png",
   },
@@ -30,7 +30,7 @@ const projects = [
     details: "PUP-STC 28th Commencement Exercises",
     period: "September 2023",
     techStack: [],
-    links: [],
+    links: {},
     category: ["design"],
     image: "/images/projects/graduation-stationery/thumbnail.png",
   },
@@ -39,13 +39,11 @@ const projects = [
     details: "Portfolio Website",
     period: "August - October 2023",
     techStack: ["HTML", "CSS", "Bootstrap", "JavaScript/jQuery"],
-    links: [
-      {
-        figma:
-          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?t=nHS6V4U1OezvHq5g-0",
-      },
-      { hosted: "https://nicolealcala.github.io/" },
-    ],
+    links: {
+      figma:
+        "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?t=nHS6V4U1OezvHq5g-0",
+      hosted: "https://nicolealcala.github.io/",
+    },
     category: ["development", "design"],
     image: "/images/projects/portfolio-v1/thumbnail.png",
   },
@@ -54,12 +52,11 @@ const projects = [
     details: "Demo Mobile App Design",
     period: "September 2023",
     techStack: [],
-    links: [
-      {
-        figma:
-          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=0-1&node-type=CANVAS&t=FJEsCrT1GI5BDUJG-0",
-      },
-    ],
+    links: {
+      figma:
+        "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=0-1&node-type=CANVAS&t=FJEsCrT1GI5BDUJG-0",
+    },
+
     category: ["design"],
     image: "/images/projects/medeco/thumbnail.png",
   },
@@ -68,12 +65,10 @@ const projects = [
     details: "Internship Project",
     period: "June 2023",
     techStack: [],
-    links: [
-      {
-        figma:
-          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=936-358&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
-      },
-    ],
+    links: {
+      figma:
+        "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=936-358&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+    },
     category: ["design"],
     image: "/images/projects/unblokc-wristband/thumbnail.png",
   },
@@ -82,13 +77,11 @@ const projects = [
     details: "Internship Project",
     period: "April - May 2023",
     techStack: ["HTML", "CSS", "JavaScript/jQuery"],
-    links: [
-      {
-        figma:
-          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2271&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
-      },
-      { hosted: "https://hackathon.theblokc.media/" },
-    ],
+    links: {
+      figma:
+        "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2271&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+      hosted: "https://hackathon.theblokc.media/",
+    },
     category: ["development", "design"],
     image: "/images/projects/unblokc-website/thumbnail.png",
   },
@@ -97,14 +90,12 @@ const projects = [
     details: "Capstone Project",
     period: "August 2022 - February 2023",
     techStack: ["PHP", "Sass", "Bootstrap", "JavaScript/jQuery", "MySQL"],
-    links: [
-      { github: "https://github.com/nicolealcala/KKIS" },
-      {
-        figma:
-          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=1-1292&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=1-1292&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
-      },
-      { hosted: "https://kkinfosystem.000webhostapp.com/" },
-    ],
+    links: {
+      github: "https://github.com/nicolealcala/KKIS",
+      figma:
+        "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=1-1292&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=1-1292&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+      hosted: "https://kkinfosystem.000webhostapp.com/",
+    },
     category: ["development", "design"],
     image: "/images/projects/kkis/thumbnail.png",
   },
@@ -113,13 +104,11 @@ const projects = [
     details: "School Project",
     period: "February - March 2022",
     techStack: ["HTML", "CSS", "Bootstrap", "JavaScript"],
-    links: [
-      { github: "https://github.com/markprongoso/LoungEscape" },
-      {
-        figma:
-          "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2274&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
-      },
-    ],
+    links: {
+      github: "https://github.com/markprongoso/LoungEscape",
+      figma:
+        "https://www.figma.com/design/mW1bi8A8LOLZBUBaK5rKnB/UI%2FUX-Projects?node-id=18-2274&node-type=CANVAS&t=HM7AvZJ6la2fxKKH-0",
+    },
     category: ["development", "design"],
     image: "/images/projects/loungescape/thumbnail.png",
   },
