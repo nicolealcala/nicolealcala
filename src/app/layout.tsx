32071e1afcc92e252692c5a783ba89ebaaef067d
Nicole Alcala <nicolealcalq@gmail.com>
2024-11-04 20:10:40 +0800

    removed baiJamjuree font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
 import { NextUIProvider } from "@nextui-org/react";
-import { Inter, Bai_Jamjuree } from "next/font/google";
-import BgLines from "@/components/ui/bg-lines";
+import { Inter } from "next/font/google";
 import "./globals.scss";
 import BackToTop from "@/components/ui/back-to-top";
 import GlobalProvider from "@/components/context-providers/global-context-provider";
 import ClientLayout from "@/components/client-pages/layout";
 
 const inter = Inter({ subsets: ["latin"] });
-export const baiJamjuree = Bai_Jamjuree({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Nicole Alcala",
