f6cf81c9cc4ef78969463d2fddf76ac28eaa63bf
Nicole Alcala <nicolealcalq@gmail.com>
2024-11-10 17:56:07 +0800

    set viewport once to true

--- src/lib/motion/props.ts
+++ src/lib/motion/props.ts
@@ -2,7 +2,7 @@
 export const fadeInUpProps = {
   initial: { opacity: 0, y: 50 },
   whileInView: { opacity: 1, y: 0 },
-  viewport: { once: false },
+  viewport: { once: true },
   transition: {
     duration: 0.5,
     delay: 0.5,
