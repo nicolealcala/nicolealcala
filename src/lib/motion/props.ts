d44550fda4b3b91e8803e145549f5d95e249c8ca
Nicole Alcala <nicolealcalq@gmail.com>
2024-11-10 17:01:54 +0800

    reusable motion fadeIn props

---
+++ src/lib/motion/props.ts
@@ -0,0 +1,10 @@
+// motionProps.js
+export const fadeInUpProps = {
+  initial: { opacity: 0, y: 50 },
+  whileInView: { opacity: 1, y: 0 },
+  viewport: { once: false },
+  transition: {
+    duration: 0.5,
+    delay: 0.5,
+  },
+};
