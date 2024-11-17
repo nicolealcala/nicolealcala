49bf173fe5b0fafdd584d1f04c3af5997f808c21
Nicole Alcala <nicolealcalq@gmail.com>
2024-11-17 16:27:22 +0800

    updated awards

--- src/components/sections/about3-awards.tsx
+++ src/components/sections/about3-awards.tsx
@@ -21,18 +21,19 @@ const Awards = () => {
                 key={i}
                 className="relative w-full h-[350px] max-w-[400px] max-h-[350px] place-self-center rounded overflow-hidden"
               >
-                <div className="overlay p-6 absolute top-0 left-0 w-full h-[350px] max-w-[400px] max-h-[350px] z-20 opacity-0 hover:opacity-100 transition duration-300 ease-in-out flex flex-col gap-y-1 items-center justify-center">
+                <div className="overlay backdrop-blur-sm p-6 absolute top-0 left-0 w-full h-[350px] max-w-[400px] max-h-[350px] z-20 opacity-0 hover:opacity-100 transition duration-300 ease-in-out flex flex-col gap-y-1 items-center justify-center">
                   <h2 className="text-center font-semibold">{award.title}</h2>
                   <p className="text-center mt-2">{award.description}</p>
                   <div className="flex gap-x-2 items-center">
-                    <p className="text-center text-sm italic">{award.date}</p> |
+                    <p className="text-center text-sm italic">{award.date}</p>
                     {award.link && (
                       <Link
                         href={award.link}
                         target="_blank"
                         className="flex gap-x-1 items-center text-sm italic"
                       >
-                        View official list <ChevronsRight />
+                        <span className="not-italic">|</span> View official list{" "}
+                        <ChevronsRight />
                       </Link>
                     )}
                   </div>
@@ -43,6 +44,7 @@ const Awards = () => {
                   src={award.image}
                   alt={award.title}
                   className="w-full h-full object-cover object-center"
+                  priority
                 />
               </div>
             ))}
