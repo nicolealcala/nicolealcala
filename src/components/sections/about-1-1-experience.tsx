0d77b7520e1481ce873324029ff90eeae2bf8cea
Nicole Alcala <nicolealcalq@gmail.com>
2024-11-17 16:56:24 +0800

    draft section for experience

---
+++ src/components/sections/about-1-1-experience.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+import Section from "../shared-ui/section-full";
+
+const Experience = () => {
+  const items = [
+    {
+      title: "January 2023 - Present",
+      cardTitle: "WPH Digital Pte Ltd",
+      cardSubtitle: "Analyst Programmer | Full-stack Web Developer",
+    },
+    {
+      title: "2019 - 2024",
+      cardTitle: "Polytechnic University of the Philippines",
+      cardSubtitle: "Bachelor of Science in Information Technology",
+    },
+  ];
+
+  return <Section className="bg-[#eff3fc]">Hello</Section>;
+};
+
+export default Experience;
