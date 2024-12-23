c2a5b39ca480411166f2f477df0a3835f82f8b9b
Nicole Alcala <nicolealcalq@gmail.com>
2024-12-23 18:54:57 +0800

    added bottom-up panel for projects

---
+++ src/components/panels/bottom-up.tsx
@@ -0,0 +1,72 @@
+"use client";
+import React from "react";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  useDisclosure,
+} from "@nextui-org/react";
+
+interface LinksObject {
+  figma?: string;
+  github?: string;
+  hosted?: string;
+}
+
+interface ProjectProp {
+  title: string;
+  details: string;
+  period: string;
+  techStack: string[];
+  links: LinksObject;
+  category: string[];
+  image: string;
+  onOpen: () => void; // Added onOpen
+  onClose: () => void; // Added onClose
+  isOpen: boolean; // Added isOpen to control drawer visibility
+}
+
+const Project = ({
+  title,
+  details,
+  period,
+  techStack,
+  links,
+  category,
+  image,
+  isOpen,
+  onOpen,
+  onClose,
+}: ProjectProp) => {
+  return (
+    <Drawer
+      isOpen={isOpen}
+      onClose={onClose}
+      placement="bottom"
+      size="5xl"
+      classNames={{
+        base: "h-[90%] !bg-white opacity-100",
+        header: "border border-b-1 h-16",
+        closeButton: "right-4 top-4 lg:right-8 lg:top-3 text-2xl",
+        body: "py-6 px-24",
+      }}
+    >
+      <DrawerContent>
+        {(onClose: any) => (
+          <>
+            {/* <DrawerHeader className="flex flex-col gap-1">
+              {project.title || "DreamLabs"}
+            </DrawerHeader> */}
+            <DrawerBody>
+              <h2 className="font-bold">{title}</h2>
+              <p>{details}</p>
+            </DrawerBody>
+          </>
+        )}
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default Project;
