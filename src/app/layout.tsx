6e8be2cdb6a7345dea1e92d6b3cccc810d8d8fa7
Nicole Alcala <nicolealcalq@gmail.com>
2024-12-23 19:20:31 +0800

    added export const runtime = "edge";

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import ClientLayout from "@/components/client-pages/layout";
 
 const inter = Inter({ subsets: ["latin"] });
 
+export const runtime = "edge";
+
 export const metadata: Metadata = {
   title: "Nicole Alcala",
   description: "Portfolio Website v2.0",
